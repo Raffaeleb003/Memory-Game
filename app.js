095d4b5118ef2bce269569e6cc3b34c9304812a4
Raffaele Balsamo <raffob003@gmail.com>
2023-01-17 18:29:32 +0100

    Images added

--- app.js
+++ app.js
@@ -48,3 +48,20 @@ const cardArray = [
       img: 'images/hotdog.png'
     }
   ]
+
+  // Sorting the cards randomly
+  cardArray.sort(() => 0.5 - Math.random())
+
+  const gridDisplay = document.querySelector('#grid')
+
+  // Create Board
+  function createBoard() {
+    for (let i = 0; i < 10; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        gridDisplay.appendChild(card)
+    }
+  }
+
+  createBoard()
